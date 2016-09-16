31b28c2101ab4f05053f79c7b111f4d13cd7236d
Brian Cavalier <brian@briancavalier.com>
2016-09-16 17:07:34 -0400

    feat(casl): Switch to functional serializer (#1)
    * Refactor to functional serializer
    
    * Remove old JSONSerializer
    
    * feat(casl): Simplify and update tests
    
    * Add jsonDeserializer, tests

--- src/index.js
+++ src/index.js
@@ -1,55 +1,45 @@
-/* global localStorage */
-
-// Create a Store that holds values in the `storage` object
-export const memoryStore = (hash, storage) => new Store(new JSONSerializer(hash), storage, '')
-
 // Create a Store that holds values in localStorage
-export const localStore = (hash) => new Store(new JSONSerializer(hash), localStorage, '')
+// The the returned store will have the value addressed by the provided key
+export const localStore = (hash, localStorage, key) =>
+  new Store(jsonSerializer(hash), jsonDeserializer, localStorage, key)
+
+export const jsonDeserializer = json =>
+  json == null ? undefined : JSON.parse(json)
+
+export const jsonSerializer = hash => data => {
+  const json = JSON.stringify(data)
+  return { key: hash(json), content: json, deserialize: jsonDeserializer }
+}
 
 // An immutable content-addressed store
 // A Store contains an immutable value that is considered to be unique
 // based on the provided serializer's key generation algorithm
 export class Store {
-  constructor (serializer, storage, key) {
-    this.serializer = serializer
+  constructor (serialize, deserialize, storage, key) {
+    this.serialize = serialize
+    this.deserialize = deserialize
     this.storage = storage
     this.key = key
   }
 
   // Get the value out of this Store
   extract () {
-    return this.serializer.deserialize(this.storage[this.key])
+    return this.deserialize(this.storage[this.key])
   }
 
   // Update the value in this store by mapping a function over it
   // Returns a Store containing the result
   map (f) {
-    return this.set(f(this.serializer.deserialize(this.storage[this.key])))
+    return this.set(f(this.extract()))
   }
 
   // Return a store containing `data` as its value
   set (data) {
-    const { key, content } = this.serializer.serialize(data)
+    const { key, content, deserialize } = this.serialize(data)
     if (key === this.key) {
       return this // the content already exists, bail out
     }
     this.storage[key] = content
-    return new Store(this.serializer, this.storage, key)
+    return new Store(this.serialize, deserialize, this.storage, key)
   }
 }
-
-export class JSONSerializer {
-  constructor (hash) {
-    this.hash = hash
-  }
-
-  serialize (data) {
-    const json = JSON.stringify(data)
-    return { key: this.hash(json), content: json }
-  }
-
-  deserialize (content) {
-    return JSON.parse(content)
-  }
-}
-
--- test/index-test.js
+++ test/index-test.js
@@ -1,17 +1,27 @@
 import { describe, it } from 'mocha'
 import assert from 'assert'
-import { JSONSerializer } from '../src/index'
+import { jsonDeserializer, jsonSerializer } from '../src/index'
 
-describe('JSONSerializer', () => {
-  describe('serialize', () => {
-    it('should return hash key and seriaized content', () => {
-      const hash = s => `test${s}`
-      const data = { test: Math.random() }
-      const s = new JSONSerializer(hash)
+describe('jsonSerializer', () => {
+  it('should return hash key and serialized content', () => {
+    const hash = s => `test${s}`
+    const data = { test: Math.random() }
+    const serialize = jsonSerializer(hash)
 
-      const { key, content } = s.serialize(data)
-      assert.strictEqual(key, hash(JSON.stringify(data)))
-      assert.deepEqual(data, JSON.parse(content))
-    })
+    const { key, content, deserialize } = serialize(data)
+    assert.strictEqual(key, hash(JSON.stringify(data)))
+    assert.deepStrictEqual(data, deserialize(content))
+  })
+})
+
+describe('jsonDeserializer', () => {
+  it('should return undefined for null or undefined input', () => {
+    assert.strictEqual(undefined, jsonDeserializer(null))
+    assert.strictEqual(undefined, jsonDeserializer(undefined))
+  })
+
+  it('should return deserialized data for value input', () => {
+    const data = { test: Math.random() }
+    assert.deepStrictEqual(data, jsonDeserializer(JSON.stringify(data)))
   })
 })
